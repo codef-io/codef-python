92a63fc11b0424e8e281d09c7518e84f94f0e063
skcrackers <skcrackers@gmail.com>
2019-07-05 12:37:57 +0900

    CODEF API sample initial commit

---
+++ TestKR_BK_1_B_001.py
@@ -0,0 +1,87 @@
+# -*- coding: utf-8 -*-
+# UTF-8 encoding when using korean
+
+import requests, json, base64
+
+# ========== HTTP 기본 함수 ==========
+
+def http_sender(url, token, body):
+    headers = {'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + token
+        }
+
+    response = requests.post(url, headers = headers, data = json.dumps(body))
+
+    print(response.text)
+    print(response.status_code)
+
+    return response
+# ========== HTTP 함수  ==========
+
+# ========== Toekn 재발급  ==========
+def request_token(url, client_id, client_secret):
+    authHeader = stringToBase64(client_id + ':' + client_secret)
+
+    headers = {
+        'Acceppt': 'application/json',
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Authorization': 'Basic ' + authHeader
+        }
+
+    response = requests.post(url, headers = headers, data = 'grant_type=client_credentials&scope=read')
+
+    print(response.text)
+    print(response.status_code)
+
+    return response
+# ========== Toekn 재발급  ==========
+
+# ========== Encode string data  ==========
+def stringToBase64(s):
+    return base64.b64encode(s.encode('utf-8'))
+
+def base64ToString(b):
+    return base64.b64decode(b).decode('utf-8')
+# ========== Encode string data  ==========
+
+# CodefURL
+codef_url = 'http://192.168.10.126:10001'
+token_url = 'http://192.168.10.126:8888/oauth/token'
+
+# 은행 법인 보유계좌
+account_list_path = '/v1/kr/bank/b/account/list'
+
+# 기 발급된 토큰
+token ='eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJzZXJ2aWNlX3R5cGUiOiIwIiwic2NvcGUiOlsicmVhZCJdLCJzZXJ2aWNlX25vIjoiOTk5OTk5OTk5OSIsImV4cCI6MTU2MjczNjUyNywiYXV0aG9yaXRpZXMiOlsiSU5TVVJBTkNFIiwiUFVCTElDIiwiQkFOSyIsIkVUQyIsIlNUT0NLIiwiQ0FSRCJdLCJqdGkiOiIyODBhNjVkOS02NjU1LTQ5MzYtODEwNS05MjUyYTk4MGRjMDgiLCJjbGllbnRfaWQiOiJjb2RlZl9tYXN0ZXIifQ.eFCEgxcntsEkjFORAWGSi6949UMOuCxVsm2wnYlDXqrHWXXwG7-XfKugsBNone_qRRGeKD3iv6f_TEcVMWyTz8aS0fRbE514LVz6PnzKbruyPNDA5Pk3ym8up9h4Ba1ix__Bvpu_TB0Y7Fikk9YHWHacJy4F_WOjr8xFP-q2egh763_LqVUzRakGQoLOTukduZ5zH5lfSO1Z9yx2cnDkY4VSM9DTbycSZuA2oQkMVpXJc0slEyWLw7WNX5E-ff3fL6ePfJvu7by_4KmgmmJkOoKBWvJ00DwrwhAa1EZmjqGPYG6RE6wxSwsu3lYeiCX-jSGm_cbKdk7YDnYxm8FKzg'
+
+# BodyData
+body = {
+    'connected_id':'9LUm.uhVQbzaangazwI0tr',
+    'organization':'0011'
+}
+
+# CODEF API 요청
+response_codef_api = http_sender(codef_url + account_list_path, token, body)
+# token error
+if response_codef_api.status_code == 401:
+    dict = json.loads(response_codef_api.text)
+    # invalid_token
+    print('error = ' + dict['error'])
+    # Cannot convert access token to JSON
+    print('error_description = ' + dict['error_description'])
+
+    # reissue token
+    response_oauth = request_token(token_url, "codef_master", "codef_master_secret");
+    if response_oauth.status_code == 200:
+        dict = json.loads(response_oauth.text)
+        # reissue_token
+        token = dict['access_token']
+        print('access_token = ' + token)
+
+        # request codef_api
+        response = http_sender(codef_url + account_list_path, token, body)
+
+        # codef_api 응답 결과
+        print('access_token = ' + token)
+else:
+    print('정상처리')
