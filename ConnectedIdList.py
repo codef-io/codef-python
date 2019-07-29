c22cafee68575df3803d4fecaae6eddb6410591d
skcrackers <skcrackers@gmail.com>
2019-07-29 18:04:32 +0900

    은행/카드 샘플코드 수정

--- ConnectedIdList.py
+++ ConnectedIdList.py
@@ -1,13 +1,13 @@
 # -*- coding: utf-8 -*-
 # UTF-8 encoding when using korean
-
+#######################################
+##      ConnectedId 목록조회
 ######################################
-##      저축 은행 개인 보유계좌
-######################################
-
-
 import requests, json, base64
 import urllib
+
+from Crypto.PublicKey import RSA
+from Crypto.Cipher import PKCS1_v1_5 as Cipher_PKCS1_v1_5
 
 # ========== HTTP 기본 함수 ==========
 
@@ -16,8 +16,9 @@ def http_sender(url, token, body):
         'Authorization': 'Bearer ' + token
         }
 
-    response = requests.post(url, headers = headers, data = json.dumps(body))
+    response = requests.post(url, headers = headers, data = urllib.quote(str(json.dumps(body))))
 
+    # print('//////////////// = ' + urllib.urlencode(json.dumps(body)))
     print('response.status_code = ' + str(response.status_code))
     print('response.text = ' + urllib.unquote_plus(response.text.encode('utf8')))
 
@@ -37,7 +38,7 @@ def request_token(url, client_id, client_secret):
     response = requests.post(url, headers = headers, data = 'grant_type=client_credentials&scope=read')
 
     print('response.status_code = ' + str(response.status_code))
-    print('response.text = ' + urllib.unquote_plus(response.text.encode('utf8')))
+    print('response.text = ' + response.text)
 
     return response
 # ========== Toekn 재발급  ==========
@@ -50,31 +51,33 @@ def base64ToString(b):
     return base64.b64decode(b).decode('utf-8')
 # ========== Encode string data  ==========
 
-# CodefURL
-codef_url = 'https://tapi.codef.io'
+# token URL
 token_url = 'https://toauth.codef.io/oauth/token'
-
-# 저축 은행 개인 보유계좌
-account_list_path = '/v1/kr/bank2/p/account/account-list'
 
 # 기 발급된 토큰
 token =''
-
-# BodyData
-body = {
-    'connectedId':'9LUm.uhVQbzaangazwI0tr',     // 엔드유저의 은행/카드사 계정 등록 후 발급받은 커넥티드아이디 예시
-    'organization':'기관코드',
-    'bankName':'<저축은행 중앙회 모듈 필수: [서브도메인 명]>'
+##############################################################################
+##                               ConnectedId 목록조회                         ##
+##############################################################################
+# Input Param
+#
+# pageNo : 페이지 번호(생략 가능) 생략시 1페이지 값(0) 자동 설정
+#
+##############################################################################
+codef_connected_id_list_url = 'https://tapi.codef.io/v1/account/connectedId-list'
+codef_connected_id_list_body = {
+    'pageNo':'5'            # 페이지 번호(생략 가능) 생략시 1페이지 값(0) 자동 설정
 }
 
-# CODEF API 요청
-response_codef_api = http_sender(codef_url + account_list_path, token, body)
-
-if response_codef_api.status_code == 200:
-    print('정상처리')
-# token error
-elif response_codef_api.status_code == 401:
-    dict = json.loads(response_codef_api.text)
+response_account_create = http_sender(codef_connected_id_list_url, token, codef_connected_id_list_body)
+if response_account_create.status_code == 200:      # success
+    dict = json.loads(urllib.unquote_plus(response_account_create.text.encode('utf8')))
+    if 'data' in dict and str(dict['data']) != '{}':
+        print('조회 정상 처리')
+    else:
+        print('조회 오류')
+elif response_account_create.status_code == 401:      # token error
+    dict = json.loads(response_account_create.text)
     # invalid_token
     print('error = ' + dict['error'])
     # Cannot convert access token to JSON
@@ -86,15 +89,16 @@ elif response_codef_api.status_code == 401:
         dict = json.loads(response_oauth.text)
         # reissue_token
         token = dict['access_token']
-        print('access_token = ' + token)
 
         # request codef_api
-        response = http_sender(codef_url + account_list_path, token, body)
-
-        # codef_api 응답 결과
-        print(response.status_code)
-        print(response.text)
+        response = http_sender(codef_connected_id_list_url, token, codef_connected_id_list_body)
+        if response.status_code == 200:      # success
+            dict = json.loads(urllib.unquote_plus(response.text.encode('utf8')))
+            if 'data' in dict and str(dict['data']) != '{}':
+                print('조회 정상 처리')
+            else:
+                print('조회 오류')
     else:
         print('토큰발급 오류')
 else:
-    print('API 요청 오류')
+    print('조회 오류')
--- RegisterAccount.py
+++ RegisterAccount.py
@@ -46,7 +46,7 @@ def request_token(url, client_id, client_secret):
     return response
 # ========== Toekn 재발급  ==========
 
-# ========== public Encrypt  ==========
+# ========== RSA Encrypt  ==========
 def publicEncRSA(publicKey, data):
     keyDER = base64.b64decode(pubKey)
     keyPub = RSA.importKey(keyDER)
@@ -57,7 +57,7 @@ def publicEncRSA(publicKey, data):
     print('encryptedData = ' + encryptedData)
 
     return encryptedData
-# ========== public Encrypt  ==========
+# ========== RSA Encrypt  ==========
 
 # ========== Encode string data  ==========
 def stringToBase64(s):
